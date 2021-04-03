425014f9005cd79226fc47ef6a4c2aa1913ce959
Shivrajsinh Rana <shivrajrana@Shivrajsinhs-MacBook-Air.local>
2021-04-03 16:33:31 +0530

    Truffle Develop Completed

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,8 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+require('babel-register');
+require('babel-polyfill');
 
 module.exports = {
   /**
@@ -42,11 +44,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -74,10 +76,12 @@ module.exports = {
     // }
   },
 
+  contracts_directory: './contracts',
+  contracts_build_directory: './build',
   // Set default mocha options here, use special reporters etc.
-  mocha: {
+  //mocha: {
     // timeout: 100000
-  },
+  //},
 
   // Configure your compilers
   compilers: {
@@ -85,12 +89,13 @@ module.exports = {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
+        optimizer: {
+          enabled: true,
+          runs: 200
       //  },
       //  evmVersion: "byzantium"
       // }
     }
   }
-};
+}
+}
